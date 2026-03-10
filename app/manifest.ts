f226c3e254db13c0d6ff2f2ba2441b1cd4a2578a
wjdxodbs <wjdxodbs52@naver.com>
2026-03-10 17:16:10 +0900

    refactor: manifest 업데이트

--- app/manifest.ts
+++ app/manifest.ts
@@ -14,15 +14,21 @@ export default function manifest(): MetadataRoute.Manifest {
     lang: "ko",
     icons: [
       {
-        src: "/icon-192.svg",
+        src: "/icon-192.png",
         sizes: "192x192",
-        type: "image/svg+xml",
-        purpose: "maskable",
+        type: "image/png",
+        purpose: "any",
       },
       {
-        src: "/icon-512.svg",
+        src: "/icon-512.png",
         sizes: "512x512",
-        type: "image/svg+xml",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/icon-512-maskable.png",
+        sizes: "512x512",
+        type: "image/png",
         purpose: "maskable",
       },
     ],
